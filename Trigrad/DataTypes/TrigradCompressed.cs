46b6d764a9894857e73f6095b5acc56903ca5ef1
ruarai <ruarai.big@gmail.com>
2015-07-04 16:59:36 +1000

    improve file size per pjtr

--- Trigrad/DataTypes/TrigradCompressed.cs
+++ Trigrad/DataTypes/TrigradCompressed.cs
@@ -52,9 +52,27 @@ namespace Trigrad.DataTypes
                 Dictionary<Point, ushort> pointIndex = new Dictionary<Point, ushort>();
 
                 ushort colors = reader.ReadUInt16();
+
+                byte[] reds = new byte[colors];
+                byte[] greens = new byte[colors];
+                byte[] blues = new byte[colors];
+
                 for (int i = 0; i < colors; i++)
                 {
-                    colorIndex.Add(Color.FromArgb(reader.ReadByte(), reader.ReadByte(), reader.ReadByte()));
+                    reds[i] = reader.ReadByte();
+                }
+                for (int i = 0; i < colors; i++)
+                {
+                    greens[i] = reader.ReadByte();
+                }
+                for (int i = 0; i < colors; i++)
+                {
+                    blues[i] = reader.ReadByte();
+                }
+
+                for (int i = 0; i < colors; i++)
+                {
+                    colorIndex.Add(Color.FromArgb(reds[i], greens[i], blues[i]));
                 }
                 uint points = reader.ReadUInt32();
                 for (int i = 0; i < points; i++)
@@ -75,14 +93,14 @@ namespace Trigrad.DataTypes
         /// <summary> Saves a TrigradCompressed image to a stream. </summary>
         public void Save(Stream s)
         {
-            using(GZipStream zipper = new GZipStream(s,CompressionLevel.Optimal))
+            using (GZipStream zipper = new GZipStream(s, CompressionLevel.Optimal))
             using (BinaryWriter writer = new BinaryWriter(zipper))
             {
                 writer.Write((ushort)Width);
                 writer.Write((ushort)Height);
 
                 List<Color> colorIndex = new List<Color>();
-                Dictionary<Point,ushort> pointIndex = new Dictionary<Point, ushort>();
+                Dictionary<Point, ushort> pointIndex = new Dictionary<Point, ushort>();
 
                 foreach (var pair in SampleTable)
                 {
@@ -93,7 +111,7 @@ namespace Trigrad.DataTypes
                     else
                     {
                         colorIndex.Add(pair.Value);
-                        pointIndex.Add(pair.Key,(ushort)(colorIndex.Count -1));
+                        pointIndex.Add(pair.Key, (ushort)(colorIndex.Count - 1));
                     }
                 }
 
@@ -102,7 +120,13 @@ namespace Trigrad.DataTypes
                 foreach (var color in colorIndex)
                 {
                     writer.Write(color.R);
+                }
+                foreach (var color in colorIndex)
+                {
                     writer.Write(color.G);
+                }
+                foreach (var color in colorIndex)
+                {
                     writer.Write(color.B);
                 }
 
