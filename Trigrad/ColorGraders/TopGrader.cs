0a2d7f4af67a19620de99d81a303e206a43ceca0
ruarai <ruarai.big@gmail.com>
2015-07-10 21:32:34 +1000

    add new graders

---
+++ Trigrad/ColorGraders/TopGrader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trigrad.ColorGraders
+{
+    public class TopGrader : IGrader 
+    {
+        public Color Grade(Color cU, Color cV, Color cW, double u, double v, double w, int x, int y)
+        {
+            int uSum = cU.R + cU.G + cU.B;
+            int vSum = cV.R + cV.G + cV.B;
+            int wSum = cW.R + cW.G + cW.B;
+
+            if (uSum > vSum && uSum > wSum)
+            {
+                return cU;
+            }
+            else if (vSum > wSum)
+                return cV;
+            else
+                return cW;
+
+        }
+    }
+}
