521e25415bbf11e33b13f1ec4b9c53ab9fc7ddb4
ruarai <ruarai.big@gmail.com>
2015-08-19 16:14:50 +1000

    remove useless grader

--- Trigrad/ColorGraders/TriGrader.cs
+++ Trigrad/ColorGraders/TriGrader.cs
@@ -8,17 +8,12 @@ using System.Threading.Tasks;
 namespace Trigrad.ColorGraders
 {
     /// <summary> Color grader that fills the triangle with an average of the three vertice samples. </summary>
-    public class PixelGrader : IGrader
+    public class TriGrader : IGrader
     {
         /// <summary> Produces a color from the specified coordinates and colors. </summary>
         public Color Grade(Color cU, Color cV, Color cW, double u, double v, double w, int x, int y, Point pU, Point pV, Point pW)
         {
-            int val = (x + y) % 4;
-            if (val == 0)
-                return cU;
-            if (val == 1)
-                return cV;
-            return cW;
+            return cU;
         }
     }
 }
