4d3622ff6020147c0fa73182a3bb04edba144dba
ruarai <ruarai.big@gmail.com>
2015-08-22 17:41:37 +1000

    add new graders

---
+++ Trigrad/ColorGraders/DistanceGrader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+
+namespace Trigrad.ColorGraders
+{
+    public class DistanceGrader : IGrader
+    {
+        /// <summary> Produces a color from the specified coordinates and colors. </summary>
+        public Color Grade(Color cU, Color cV, Color cW, double u, double v, double w, int x, int y, Point pU, Point pV, Point pW)
+        {
+            double distU = dist(new Point(x, y), pU);
+            double distV = dist(new Point(x, y), pV);
+            double distW = dist(new Point(x, y), pW);
+
+
+            if (distU < distV && distU < distW)
+                return cU;
+            if (distV < distW)
+                return cV;
+            return cW;
+        }
+
+        private double dist(Point a, Point b)
+        {
+            int dX = a.X - b.X;
+            int dY = a.Y - b.Y;
+
+            if (dX > dY)
+                return dX;
+            return dY;
+        }
+    }
+}
--- Trigrad/ColorGraders/DitherGrader.cs
+++ Trigrad/ColorGraders/DitherGrader.cs
@@ -13,14 +13,24 @@ namespace Trigrad.ColorGraders
         /// <summary> Produces a color from the specified coordinates and colors. </summary>
         public Color Grade(Color cU, Color cV, Color cW, double u, double v, double w, int x, int y, Point pU, Point pV, Point pW)
         {
-            int val = (x + y) % 4;
+            if (u >= 0.9)
+                return cU;
+            if (v >= 0.9)
+                return cV;
+            if (w >= 0.9)
+                return cW;
+
+
+            int valU = (int)(x + y + u * 79) % 4;
+            int valV = (int)(x + y + v * 79) % 4;
+            int valW = (int)(x + y + w * 79) % 4;
 
             if (u >= v && u >= w)
-                return ditherFurther(cU, cV, cW, val);
+                return ditherFurther(cU, cV, cW, valU);
             if (v >= w)
-                return ditherFurther(cV, cU, cW, val);
+                return ditherFurther(cV, cU, cW, valV);
 
-            return ditherFurther(cW, cV, cU, val);
+            return ditherFurther(cW, cV, cU, valW);
         }
         private static Color ditherFurther(Color a, Color b, Color c, int val)
         {
