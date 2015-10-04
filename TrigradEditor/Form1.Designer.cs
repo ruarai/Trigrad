fcb3bd8ecf3a5eab2c63e1d77de4cd0f4e042b7e
ruarai <ruarai.big@gmail.com>
2015-10-04 11:56:50 +1100

    add beta editor

---
+++ TrigradEditor/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace TrigradEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.outputPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // outputPictureBox
+            // 
+            this.outputPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.outputPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.outputPictureBox.Name = "outputPictureBox";
+            this.outputPictureBox.Size = new System.Drawing.Size(752, 730);
+            this.outputPictureBox.TabIndex = 0;
+            this.outputPictureBox.TabStop = false;
+            this.outputPictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.outputPictureBox_MouseDown);
+            this.outputPictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.outputPictureBox_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 730);
+            this.Controls.Add(this.outputPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.outputPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox outputPictureBox;
+    }
+}
+
---
+++ TrigradEditor/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Trigrad;
+using Trigrad.ColorGraders;
+using Trigrad.DataTypes;
+using Trigrad.DataTypes.Compression;
+
+namespace TrigradEditor
+{
+    public partial class Form1 : Form
+    {
+        private TrigradCompressed meshImage;
+        private TrigradOptions options;
+        public Form1()
+        {
+            InitializeComponent();
+
+            string input = "tests\\input\\Art.jpg";
+
+            PixelMap inputBitmap = PixelMap.SlowLoad(new Bitmap(input));
+            FrequencyTable table = new FrequencyTable(inputBitmap, 1, 0);
+
+            options = new TrigradOptions { SampleCount = 10000, FrequencyTable = table, ScaleFactor = 1, Resamples = 30, Iterations = 1, Grader = new BarycentricGrader() };
+
+            meshImage = TrigradCompressor.CompressBitmap(inputBitmap, options);
+            meshImage.Mesh = MeshBuilder.BuildMesh(meshImage.SampleTable);
+
+            render();
+        }
+
+        private void render()
+        {
+            GPUT.CalculateMesh(meshImage.Mesh);
+
+            var returned = TrigradDecompressor.DecompressBitmap(meshImage, options);
+
+            outputPictureBox.Image = returned.Output.Bitmap;
+        }
+
+        private Point mouseDownPoint = Point.Empty;
+
+        private void outputPictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDownPoint = e.Location;
+        }
+
+        private static double dist(Point a, Point b)
+        {
+            int dx = a.X - b.X;
+            int dy = a.Y - b.Y;
+
+            return Math.Sqrt(dx*dx + dy*dy);
+        }
+
+        private void outputPictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            Point p = new Point(e.X, e.Y);
+
+
+            List<SampleTri> tris = new List<SampleTri>();
+
+
+            foreach (var tri in meshImage.Mesh)
+            {
+                double dist = Form1.dist(p, tri.CenterPoint);
+
+                if (dist < 10d)
+                {
+                    tris.Add(tri);
+                }
+            }
+
+            foreach (var tri in tris)
+            {
+                foreach (var sample in tri.Samples)
+                {
+                    sample.Color = Color.White;
+                }
+            }
+
+            render();
+        }
+    }
+}
